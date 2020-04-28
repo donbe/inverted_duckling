dcc5b0db08f9e5230dcff0d58f0dd6b23f573b61
donbe <donbe520@gmail.com>
2020-04-28 11:20:11 +0800

    使用新的公式做旋转

--- InvertedDuckling/InvertedDuckling/DucklingView.h
+++ InvertedDuckling/InvertedDuckling/DucklingView.h
@@ -14,9 +14,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property(nonatomic)float clock;
 
-+(CGPoint)point:(CGPoint)point angle:(CGFloat)angle;
-
-
 @end
 
 NS_ASSUME_NONNULL_END
--- InvertedDuckling/InvertedDuckling/DucklingView.m
+++ InvertedDuckling/InvertedDuckling/DucklingView.m
@@ -9,7 +9,7 @@
 #import "DucklingView.h"
 
 
-static CGFloat interval_x = 5;// 旋转后的横向间距
+static CGFloat interval_x = 0;// 旋转后的横向间距
 static CGFloat interval_y = 0;// 上下两行的间距
 
 @interface DucklingView()
@@ -42,17 +42,24 @@ static CGFloat interval_y = 0;// 上下两行的间距
 
 
 -(CGRect )drawItem:(NSDictionary *)item //当前行属性
-         headItem:(NSDictionary *)headItem //第一行字属性，这一行字会影响到转场动画
+           preItem:(NSDictionary *)preItem //前一行字的属性，这一行影响了当前行的位置
           preFrame:(CGRect)preFrame //前一行字的frame
              index:(NSInteger)index //当前第几行字
-         timeScale:(float)timeScale //处于转场过渡的几分之几,值 0-1 之间
+   tstionAnimation:(NSInteger)tstionAnimation //处于转场百分比,值 0-1 之间
+         tstionScale:(float)tstionScale //处于转场百分比,值 0-1 之间
 {
     
+    // 转场动画
+    if (index == 0) {
+        tstionAnimation = [item[@"transition_animation"] intValue];
+    }
     NSString *text = item[@"text"];
     NSString *color = item[@"color"];
-    NSInteger transitionAnimation = [item[@"transition_animation"] intValue];
     CGFloat fontSize = [self estimateFontSize:text];
-    CGFloat traFontSize = fontSize *timeScale; //转场中间态字体大小
+    CGFloat tstionFontSize = fontSize *tstionScale; //转场中间态字体大小
+    
+    
+    
     
     
     // 终态计算
@@ -68,14 +75,14 @@ static CGFloat interval_y = 0;// 上下两行的间距
     
     
     // 中间态计算
-    NSDictionary* traStringAttrs = @{
-        NSFontAttributeName : [UIFont systemFontOfSize:traFontSize],
+    NSDictionary* tstionStringAttrs = @{
+        NSFontAttributeName : [UIFont systemFontOfSize:tstionFontSize],
         NSForegroundColorAttributeName : [UIColor colorFromHexAlphaString:color]
     };
-    NSAttributedString* traAttrStr = [[NSAttributedString alloc] initWithString:text attributes:traStringAttrs];
-    CGRect traRect = [text boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)
+    NSAttributedString* tstionAttrStr = [[NSAttributedString alloc] initWithString:text attributes:tstionStringAttrs];
+    CGRect tstionRect = [text boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)
        options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
-    attributes:traStringAttrs
+    attributes:tstionStringAttrs
        context:nil];
     
     
@@ -83,44 +90,41 @@ static CGFloat interval_y = 0;// 上下两行的间距
     // 其他行只需要处理缩放转场
     
     
-    switch (transitionAnimation) {
+    switch (tstionAnimation) {
+        // 旋转需要处理 第一行，第二行
         case 1://左旋
         case 2://右旋
         {
             if (index == 0){
-                float x, y, angle;
-                if (transitionAnimation == 1) {
-                    x = - rect.size.width/2;
-                    y = rect.size.width/2 - rect.size.height;
-                    angle = 90;
+                float angle;
+                CGPoint newOriginPoint;
+                if (tstionAnimation == 1) {
+                    angle = M_PI_2;
                     
-                    // 以字的左上角进行旋转
-                    CGFloat defangle = atan(rect.size.height/(rect.size.width/2));
-                    angle = angle - defangle;
-                    angle *= 1-timeScale;
-                    CGPoint point = [DucklingView point:CGPointMake(rect.size.width/2, rect.size.height) angle:angle];
-                    x = point.x - rect.size.width/2;
-                    y = point.y - rect.size.height;
+                    //计算当前所需旋转弧度
+                    angle *= 1-tstionScale;
+                    
+                    // 左上角坐标
+                    CGPoint ulp = CGPointMake(-rect.size.width/2, -rect.size.height);
+                    
+                    // 计算以字的左上角为原点对坐标轴旋转后的坐标
+                    newOriginPoint = [DucklingView rotatePoint:CGPointZero basePoint:ulp angle:angle];
                 }else{
-                    x = rect.size.width/2;
-                    y = rect.size.width/2 - rect.size.height;
-                    angle = -90;
+                    angle = -M_PI_2;
                     
-                    // 以字的右上角进行旋转
-                    CGFloat defangle = atan(rect.size.height/(-rect.size.width/2));
-                    angle = angle + defangle;
-                    angle *= 1-timeScale;
-                    CGPoint point = [DucklingView point:CGPointMake(-rect.size.width/2, rect.size.height) angle:angle];
-                    x = point.x + rect.size.width/2;
-                    y = point.y - rect.size.height;
+                    //计算当前所需旋转弧度
+                    angle *= 1-tstionScale;
+                    
+                    // 右上角坐标
+                    CGPoint urp = CGPointMake(rect.size.width/2, -rect.size.height);
+                    
+                    // 计算以字的右上角为原点对坐标轴旋转后的坐标
+                    newOriginPoint = [DucklingView rotatePoint:CGPointZero basePoint:urp angle:angle];
                 }
                 
-                
-                
-                
-                // 移动+旋转
-                CGContextTranslateCTM(UIGraphicsGetCurrentContext(),x,y);
-                CGContextRotateCTM (UIGraphicsGetCurrentContext(), angle * (M_PI / 180));
+                // 坐标系移动+旋转
+                CGContextTranslateCTM(UIGraphicsGetCurrentContext(),newOriginPoint.x,newOriginPoint.y);
+                CGContextRotateCTM (UIGraphicsGetCurrentContext(), angle);
                 
                 // 计算顶点坐标
                 rect.origin.x = -rect.size.width/2;
@@ -129,39 +133,43 @@ static CGFloat interval_y = 0;// 上下两行的间距
                 // 开始写字
                 [attrStr drawAtPoint:CGPointMake(rect.origin.x, rect.origin.y)];
                 
-                // 反操作，转回来，移回来，才能正确的画下一行
-                CGContextRotateCTM (UIGraphicsGetCurrentContext(), -angle * (M_PI / 180));
-                CGContextTranslateCTM(UIGraphicsGetCurrentContext(),-x,-y);
+                // 反操作，坐标系转回来，移回来，才能正确的画下一行
+                CGContextRotateCTM (UIGraphicsGetCurrentContext(), -angle);
+                CGContextTranslateCTM(UIGraphicsGetCurrentContext(),-newOriginPoint.x,-newOriginPoint.y);
+            
+            }else if(index == 1){ // 第一行字也需要处理旋转
+                
+                // 计算原点最终的点和字角度旋转
+                CGPoint newOriginPoint;
+                float  angle;
+                
+                if (tstionAnimation == 1) { //左旋
+                    newOriginPoint.x = preFrame.origin.x - interval_x - rect.size.height/2;
+                    newOriginPoint.y = preFrame.origin.y + preFrame.size.height - rect.size.width/2;
+                    angle = -M_PI_2;
+                }else{//右旋
+                    newOriginPoint.x = preFrame.origin.x + preFrame.size.width + interval_x+rect.size.height/2;
+                    newOriginPoint.y = preFrame.origin.y + preFrame.size.height - rect.size.width/2;
+                    angle = M_PI_2;
+                }
+                
+                // 计算中间态
+                newOriginPoint.x *= tstionScale;
+                newOriginPoint.y *= tstionScale;
+                angle *= tstionScale;
+                
+                //坐标系的移动和旋转
+                CGContextTranslateCTM(UIGraphicsGetCurrentContext(),newOriginPoint.x,newOriginPoint.y);
+                CGContextRotateCTM (UIGraphicsGetCurrentContext(), angle);
+                
+                // 计算顶点坐标
+                rect.origin.x = -rect.size.width/2;
+                rect.origin.y = - interval_y - rect.size.height;
+                 
+                // 开始写字
+                [attrStr drawAtPoint:CGPointMake(rect.origin.x, rect.origin.y)];
             }else{
-                float x, y, angle;
-                if (transitionAnimation == 1) {
-                    x = preFrame.origin.x - interval_x - rect.size.height/2;
-                    y = preFrame.origin.y + preFrame.size.height - rect.size.width/2;
-                    angle = -90;
-                }else{
-                    x = preFrame.origin.x + preFrame.size.width + interval_x+rect.size.height/2;
-                    y = preFrame.origin.y + preFrame.size.height - rect.size.width/2;
-                    angle = 90;
-                }
-                
-                // 只有第二条才可能存在中间态
-                if (index == 1) {
-                    x *= timeScale;
-                    y *= timeScale;
-                    angle *= timeScale;
-                }
-                
-                CGContextTranslateCTM(UIGraphicsGetCurrentContext(),x,y);
-                CGContextRotateCTM (UIGraphicsGetCurrentContext(), angle * (M_PI / 180));
-                preFrame = CGRectZero;
-                
-                // 计算顶点坐标
-                rect.origin.x = -rect.size.width/2;
-                rect.origin.y = preFrame.origin.y - interval_y - rect.size.height;
-                 
-                
-                // 开始写字
-                [attrStr drawAtPoint:CGPointMake(rect.origin.x, rect.origin.y)];
+                //其他行不用处理这个状态
             }
             
             
@@ -207,7 +215,14 @@ static CGFloat interval_y = 0;// 上下两行的间距
     CGRect frame = CGRectZero;
     
     for(int i=0;i<[items count];i++){
-        frame = [self drawItem:items[i] headItem:items[0] preFrame:frame index:i timeScale:self.clock];
+        NSInteger tstionAnimation = [items[0][@"transition_animation"] intValue];
+
+        if (i>0) {
+            frame = [self drawItem:items[i]  preItem:items[i-1] preFrame:frame index:i tstionAnimation:tstionAnimation tstionScale:self.clock];
+        }else{
+            frame = [self drawItem:items[i]  preItem:nil preFrame:frame index:i tstionAnimation:tstionAnimation tstionScale:self.clock];
+        }
+        
     }
 
 }
@@ -237,15 +252,28 @@ static CGFloat interval_y = 0;// 上下两行的间距
                                          options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
                                       attributes:@{ NSFontAttributeName : [UIFont systemFontOfSize:10] }
                                          context:nil];
-    CGFloat fontSize = floor(10 * ((self.frame.size.width - 100) / tempRect.size.width));
+    CGFloat fontSize = floor(10 * ((self.frame.size.width - 150) / tempRect.size.width));
     return fontSize;
 }
 
-+(CGPoint)point:(CGPoint)point angle:(CGFloat)angle{
-    CGFloat angle1 = angle*M_PI_2/90.0;
-    float x = point.x *cos(angle1) - point.y*sin(angle1);
-    float y = point.y*cos(angle1) + point.x*sin(angle1);
-    return CGPointMake(x, y);
+
+
+
+/// 计算 p1 围绕 p2 旋转 angle个弧度后的新坐标
+/// @param p1 旋转点
+/// @param p2 中心点
+/// @param angle 弧度 , 正数为逆时针，负数为顺时针
+/// @return 移动后的新坐标
++(CGPoint)rotatePoint:(CGPoint)p1 basePoint:(CGPoint)p2 angle:(CGFloat)angle{
+    
+    // 计算 p1 - p2
+    CGPoint ptemp = CGPointMake(p1.x-p2.x, p1.y-p2.y);
+    
+    float destx = ptemp.x *cos(angle) - ptemp.y*sin(angle);
+    float desty = ptemp.y*cos(angle) + ptemp.x*sin(angle);
+    
+    // 计算 destination + p2
+    return CGPointMake(destx+p2.x, desty+p2.y);
 }
 
 
